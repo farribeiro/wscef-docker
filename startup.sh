2759e0e74be8d6364d9c1e70d23fb20bc62f1ac8
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2017-04-02 10:29:27 -0300

    Standard the escape with the Dockerfile

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 
 if [ ! -d ${Home}/.mozilla ]
 then
-  firefox -CreateProfile default \&&
-  su -c "apt -y install /src/GBPCEFwr64.deb" \&&
+  firefox -CreateProfile default \
+  && su -c "apt -y install /src/GBPCEFwr64.deb"
 else
-  su -c "/etc/init.d/warsaw start" \&&
-  /usr/local/bin/warsaw/core \&&
-  firefox
+  su -c "/etc/init.d/warsaw start" \
+  && /usr/local/bin/warsaw/core \
+  && firefox
 fi
