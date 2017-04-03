924e71ac6f2568cc90c9a83ed5db277c24a1ca54
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2017-04-02 23:41:51 -0300

    Change of restart of container

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@
 #	--device /dev/snd \ # so we have sound
 #	-v /dev/shm:/dev/shm \
 #	--name ws-cef \
+#	--restart=on-failure:1
 #	farribeiro/wscef-docker
 #
 # Other run:
--- startup.sh
+++ startup.sh
@@ -6,7 +6,8 @@ su -c "apt-get update \
 if [ ! -d ${Home}/.mozilla ]
 then
   firefox -CreateProfile default \
-  && su -c "apt -y install /src/GBPCEFwr64.deb"
+  && su -c "apt -y install /src/GBPCEFwr64.deb" \
+  && exit 3
 else
   su -c "/etc/init.d/warsaw start" \
   && /usr/local/bin/warsaw/core \
