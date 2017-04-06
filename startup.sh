9a91b1915909d26c5ffe9782db6809954d6c9b67
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2017-04-06 18:17:15 -0300

    Isn't needed restart container anymore

--- startup.sh
+++ startup.sh
@@ -17,10 +17,10 @@ fi
 if [ ! -d ~/.mozilla ]
 then
   firefox -CreateProfile default \
-  && su -c "apt update && apt -y upgrade && apt -y install /src/GBPCEFwr64.deb" \
-  && exit 3
+  && su -c "apt update && apt -y upgrade && apt -y install /src/GBPCEFwr64.deb"
 else
-  su -c "/etc/init.d/warsaw start" \
-  && /usr/local/bin/warsaw/core \
-  && firefox -private-window www.caixa.gov.br
+  su -c "/etc/init.d/warsaw start"
 fi
+
+/usr/local/bin/warsaw/core \
+&& firefox -private-window www.caixa.gov.br
